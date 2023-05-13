8ea94dca1441e0d732a69fa1972685d58a4c8c57
Waleed-Abdullah <w26waleed@gmail.com>
2023-05-13 13:11:51 +0500

    created basic central server and storage server

--- centralServer.py
+++ centralServer.py
@@ -9,7 +9,7 @@ class FileServer:
         self.socket.bind(('', port))
         self.socket.listen(5)
         self.log = None  # to be used for logs
-        self.storageNodes = [('127.0.0.1', 5001)]
+        self.storageNodes = [('127.0.0.1', 8000)]
         print(f"Server listening on port {port}")
 
     def start(self):
@@ -30,6 +30,7 @@ class FileServer:
         try:
             conn.sendall('Enter userId: '.encode())
             userId = conn.recv(1024).decode()
+            userStorageNode = simple_hash(userId, len(self.storageNodes))
             conn.sendall(f'Thank you for loggin in user {userId}'.encode())
             while True:
                 data = conn.recv(1024).decode()
@@ -37,24 +38,53 @@ class FileServer:
                     print('exiting from ', addr)
                     break
                 # Handle the client request
-                response = self.handle_request(data, userId)
+                response = self.handle_request(data, userId, userStorageNode)
                 conn.sendall(response.encode())
             conn.close()
         except ConnectionResetError:
             print('Connection disrupted')
 
-    def handle_request(self, data, userId):
+    def handle_request(self, data, userId, userStorageNode):
         # Parse the client request and return the response
         splittedRequest = data.split()
         command = splittedRequest[0]
         match command:
-            case 'create':
-                pass
             case 'ls':
-                pass
+                if len(splittedRequest) != 1:
+                    return 'command Usage(lists the entire storage area for user): ls'
+                return self.request_storage_node(f'{userId}:ls', userStorageNode)
             case 'create':
-    def request_storage_node(self, userId, message):
-        storageNodeIp, storageNodePort = self.storageNodes(simple_hash(userId))
+                if len(splittedRequest) != 3:
+                    return 'command Usage: create type<dir,file> path'
+                type, path = splittedRequest[1], splittedRequest[2]
+                return self.request_storage_node(f'{userId}:create:{type}:{path}', userStorageNode)
+            case 'write':
+                if len(splittedRequest) < 3:
+                    return 'command Usage: write path\\to\\file data'
+                filePath = splittedRequest[1]
+                fileData = ' '.join(splittedRequest[2:])
+                return self.request_storage_node(f'{userId}:write:{filePath}:{fileData}', userStorageNode)
+            case 'read':
+                if len(splittedRequest) != 2:
+                    return 'command Usage: read path\\to\\file'
+                filePath = splittedRequest[1]
+                return self.request_storage_node(f'{userId}:read:{filePath}', userStorageNode)
+            case 'delete':
+                if len(splittedRequest) != 2:
+                    return 'command Usage: delete path\\to\\file_or_dir'
+                path = splittedRequest[1]
+                return self.request_storage_node(f'{userId}:delete:{path}', userStorageNode)
+            case 'rename':
+                if len(splittedRequest) != 3:
+                    return 'command Usage: rename path\\to\\file_or_dir new_name'
+                path = splittedRequest[1]
+                newName = splittedRequest[2]
+                return self.request_storage_node(f'{userId}:rename:{path}:{newName}')
+            case other:
+                return 'invalid request'
+
+    def request_storage_node(self, message, userStorageNode):
+        storageNodeIp, storageNodePort = self.storageNodes(userStorageNode)
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((storageNodeIp, storageNodePort))
             s.sendall(message.encode())
--- storageServer.py
+++ storageServer.py
@@ -1,6 +1,7 @@
 import os
 from socket import *
 import threading
+import sys
 
 
 class StorageServer:
@@ -11,7 +12,12 @@ class StorageServer:
         self.server_socket = socket(AF_INET, SOCK_STREAM)
         self.server_socket.bind((self.host, self.port))
 
+    def create_root_dir(self):
+        if not os.path.exists(self.root_dir):
+            os.mkdir(self.root_dir)
+
     def start(self):
+        self.create_root_dir()
         self.server_socket.listen(5)
         print(f'Storage server started on {self.host}:{self.port}')
 
@@ -25,28 +31,56 @@ class StorageServer:
 
     def handle_client(self, client_socket):
         request = client_socket.recv(1024).decode()
-        request_type, filename, data = request.split('||')
-        if request_type == 'PUT':
-            self.put_file(filename, data)
-            response = 'File stored successfully!'
-        elif request_type == 'GET':
-            file_data = self.get_file(filename)
-            if file_data:
-                response = file_data
-            else:
-                response = 'File not found'
-        else:
-            response = 'Invalid request'
+        splittedRequest = request.split(':')
+        userId, command = splittedRequest[0], splittedRequest[1]
+        userBasePath = self.get_user_base_path(userId)
+        match command:
+            case 'ls':
+                response = self.list_directory_structure(userBasePath)
+            case 'create':
+                pass
+            case 'write':
+                pass
+            case 'read':
+                pass
+            case 'delete':
+                pass
+            case 'rename':
+                pass
+            case other:
+                response = 'invalid Request'
         client_socket.send(response.encode())
         client_socket.close()
 
-    def get_file(self, filename):
+    def get_user_base_path(self, userId):
+        userBasePath = f'{self.root_dir}\\{userId}_DIR'
+        if not os.path.exists(userBasePath):
+            os.mkdir(userBasePath)
+        return userBasePath
+
+    def get_file(self, userBasePath, pathToFile):
         try:
-            with open(os.path.join(self.root_dir, filename), 'rb') as f:
-                return f.read()
+            with open(os.path.join(userBasePath, pathToFile), 'rb') as f:
+                return f.read().decode()
         except FileNotFoundError:
             return None
 
-    def put_file(self, filename, data):
-        with open(os.path.join(self.root_dir, filename), 'wb') as f:
-            f.write(data)
+    def put_file(self, userBasePath, pathToFile,  data):
+        with open(os.path.join(userBasePath, pathToFile), 'wb') as f:
+            f.write(data.encode())
+
+    def list_directory_structure(self, path):
+        outputString = ''
+        for root, dirs, files in os.walk(path):
+            for name in files:
+                outputString += os.path.join(root, name) + '\n'
+            for name in dirs:
+                outputString += os.path.join(root, name) + '\n'
+
+
+if __name__ == '__main__':
+    if len(sys.argv < 3):
+        print('Enter rootDirForServer and portNo.')
+        exit()
+    rootDir, portNo = sys.argv[1], int(sys.argv[2])
+    storageServer = StorageServer(rootDir, portNo)
